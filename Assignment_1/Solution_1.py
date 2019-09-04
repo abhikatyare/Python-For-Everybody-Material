3beafae05ecb2ddb720d50d229d0c16c9871ee1a
v-abkaty <v-abkaty@microsoft.com>
2019-09-04 21:00:11 +0530

    Adding solution for the first Assignment

--- Assignment_1/Solution_1.py
+++ Assignment_1/Solution_1.py
@@ -1,5 +1,6 @@
 import re
 
-x = "From: v-abkaty@microsoft.com"
-y = re.findall(r'^From: (\S+@\S+)',x)
-print(y)
+FILE_PATH = r"C:\Users\v-abkaty\Desktop\Workspace\Python_Course\Assignment_1\actual_data.txt"
+FILE_DATA = open(FILE_PATH,"r+")
+
+print( sum([int(x) for x in re.findall(r'[0-9]+',FILE_DATA.read())]))
