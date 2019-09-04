7911c5866217754748feee013122dc3966c9fdff
v-abkaty <v-abkaty@microsoft.com>
2019-09-04 23:25:07 +0530

    Assignment 1 with screenshots

--- Assignment_1/Solution_1.py
+++ Assignment_1/Solution_1.py
@@ -1,6 +1,4 @@
-import re
+# Author : Abhishek Katyare
+# Date   : 4th September, 2019
 
-FILE_PATH = r"C:\Users\v-abkaty\Desktop\Workspace\Python_Course\Assignment_1\actual_data.txt"
-FILE_DATA = open(FILE_PATH,"r+")
-
-print( sum([int(x) for x in re.findall(r'[0-9]+',FILE_DATA.read())]))
+print("Hello World!")
